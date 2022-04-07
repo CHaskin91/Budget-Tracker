4ed5123095d40c1c3c162c35a4cc00a6b425e970
Chubbs91 <chaskin1991@outlook.com>
2022-04-07 14:43:00 -0500

    updates to idb.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -3,3 +3,24 @@ let db;
 
 // establish a connection to IndexedDB database called "budget"
 const request = indexedDB.open("budget", 1);
+
+// this event will emit if the database version changes
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    // create object store called "pending" and set autoIncrement to true
+    db.createObjectStore("pending", { autoIncrement: true });
+};
+
+// upon a successful request
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    // check if app is online before reading from DB
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+request.onerror = function(event) {
+    console.log("Error: " + event.target.errorCode);
+};
