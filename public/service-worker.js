bcd7930030f7ecfb43b367f29e9e4f573830a449
Chubbs91 <chaskin1991@outlook.com>
2022-04-08 16:02:29 -0500

    service-worker activation event added

--- public/service-worker.js
+++ public/service-worker.js
@@ -23,3 +23,20 @@ self.addEventListener("install", function (e) {
 });
 
 // Activate
+self.addEventListener("activate", function (e) {
+    e.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
