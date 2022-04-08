ae022abb50d31d125f59bf8f12bc48558eba98e6
Chubbs91 <chaskin1991@outlook.com>
2022-04-08 15:57:11 -0500

    service-worker installed

---
+++ public/service-worker.js
@@ -0,0 +1,25 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/index.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// Install
+self.addEventListener("install", function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    self.skipWaiting();
+});
+
+// Activate
